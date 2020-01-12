6a4eb35b26c6157485b53b3b3570023bf1c90427
Chaitanya Penjuri <chaitanya.penjuri95@gmail.com>
2020-01-12 11:59:15 +0530

    Login and signup template design finish

---
+++ app/src/main/java/com/chaitupenjudcoder/projecttemplates/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.chaitupenjudcoder.projecttemplates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/chaitupenjudcoder/projecttemplates/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.chaitupenjudcoder.projecttemplates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
